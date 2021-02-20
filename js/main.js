52afb7a1cf36985ec9f44e4f12f9c0424183292d
yurim <yurim2222@gmail.com>
2021-02-20 14:47:06 +0900

    색감변경, 메인화면 풀페이지 처리

--- js/main.js
+++ js/main.js
@@ -1,13 +1,37 @@
 $(document).ready(function(){
+    var page01 = '.main';
+    var page02 = '#list';
+        var h = $(window).height();
+        $(page01).outerHeight(h);
+        $(page02).outerHeight(h);
+});
+
+
+$(document).ready(function(){
     var speed = 1000;
     var easing = 'swing';
-    var btn = '.main .click';
+    var bbtn = '.main .click';
 
-    $(btn).click(function(){
+    $(bbtn).click(function(){
 
         var pageHeight = $('.main').outerHeight();
         $('html, body').animate({
             scrollTop: pageHeight
+        },speed,easing);
+
+    });
+
+});
+
+$(document).ready(function(){
+    var speed = 1000;
+    var easing = 'swing';
+    var tbtn = '#list .arrow a';
+
+    $(tbtn).click(function(){
+
+        $('html, body').animate({
+            scrollTop: 0
         },speed,easing);
 
     });
