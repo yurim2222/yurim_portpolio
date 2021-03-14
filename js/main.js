54b4ee117b8875df62394a0d36c5abdede995623
yurim <yurim2222@gmail.com>
2021-03-14 16:30:14 +0900

    페이지 요소추가, 메인화면에 타이핑 효과 추가

--- js/main.js
+++ js/main.js
@@ -37,3 +37,42 @@ $(document).ready(function(){
     });
 
 });
+
+$(document).ready(function(){
+    let typingBool = false;
+    let typingIdx = 0;
+    let liIndex = 0;
+    let typingText = ".typingText > ul > li";
+    let liLength = $(typingText).length;
+    
+    let typingTxt = $(typingText).eq(liIndex).text(); //eq: 배열반환
+    typingTxt = typingTxt.split(""); //split: 문자열을 분해하여 배열을 반환 => "":한글자씩 자름
+    if(typingBool === false){
+        typingBool === true;
+    }
+    let tyInt = setInterval(typing,200);
+    
+    function typing(){
+        if(typingIdx < typingTxt.length){
+            $(".typing").append(typingTxt[typingIdx]);
+            typingIdx++;
+        }else{
+            if(liIndex >= liLength-1){
+                liIndex=0;
+            }else{
+                liIndex++;
+            }
+
+            typingIdx=0;
+            typingBool = false;
+            typingTxt = $(typingText).eq(liIndex).text();
+
+            clearInterval(tyInt);
+            
+            setTimeout(function(){
+                $(".typing").html('');
+                tyInt = setInterval(typing,200);
+            },1000)
+        }
+    }
+});
